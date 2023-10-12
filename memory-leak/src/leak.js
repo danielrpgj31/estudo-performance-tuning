dd499ba3812e866f1d80577398c028c7379e7493
Daniel Ribeiro <daniel.junior@gmail.com>
2023-10-11 22:13:43 -0300

    Update

--- memory-leak/src/leak.js
+++ memory-leak/src/leak.js
@@ -1,10 +1,13 @@
+function preencheMatriz = (data) => {
+  for (let i = 0; i < 10000; i++) {
+    data.push(new Array(1000000).join('x'));
+  }
+  console.log('Memoria alocada:', process.memoryUsage().heapUsed / 1024 / 1024, 'MB');
+}
 const memoryLeak = () => {
   const data = [];
   setInterval(() => {
-    for (let i = 0; i < 10000; i++) {
-      data.push(new Array(1000000).join('x'));
-    }
-    console.log('Memória alocada:', process.memoryUsage().heapUsed / 1024 / 1024, 'MB');
+    preencheMatriz(data);
   }, 1000);
 };
 
