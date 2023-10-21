d1840f5490ef6b1920d9d173f755f10729852332
Daniel Ribeiro <daniel.junior@gmail.com>
2023-10-21 20:40:02 -0300

    Update

--- apm/nodejs-agent/index.js
+++ apm/nodejs-agent/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const parserLib = require('./parser')
 const promBundle = require('express-prom-bundle');
+const promClient = require('prom-client');
+const ParserFile = require('./parser');
 const metricsMiddleware = promBundle({ includeMethod: true });
 const app = express();
 const port = 7001;
 
-app.use(metricsMiddleware);
+ParserFile();
 
-const promClient = require('prom-client');
+app.use(metricsMiddleware);
 
 // Crie uma métrica do tipo "gauge"
 const customGauge = new promClient.Gauge({
@@ -17,12 +20,20 @@ const customGauge = new promClient.Gauge({
 
 // Endpoint para atualizar a métrica do tipo "gauge"
 app.get('/update-gauge', (req, res) => {
+  const parser = ParserFile;
   const value = Math.random() * 100; // Valor aleatório para o exemplo
   const label = 'example';
   customGauge.set({ label }, value);
+    
+  parser();
+
   res.send(`Métrica do tipo "gauge" atualizada: ${value}`);
 });
 
+/*
 app.listen(port, () => {
   console.log(`Exporter do Prometheus está rodando na porta ${port}`);
 });
+*/
+
+
