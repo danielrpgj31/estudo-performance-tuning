248f1c285d73cf14a50484615f2f873057464572
Daniel Ribeiro <daniel.junior@gmail.com>
2023-10-26 17:58:15 -0300

    Update

--- memoryleak/src/leak.js
+++ memoryleak/src/leak.js
@@ -1,39 +1,37 @@
-const express = require('express');
-const MonitorGC = require('./diagnostics/gc');
+const express = require("express");
+const MonitorGC = require("./diagnostics/gc");
 
 const callbackMemoryLeak = () => {
-
   for (let i = 0; i < maxIterador; i++) {
-    data.push(new Array(tamanhoArray).join('x'));
+    data.push(new Array(tamanhoArray).join("x"));
   }
-  console.log('Memoria alocada:', process.memoryUsage().heapUsed / 1024 / 1024, 'MB');
+  //console.log('Memoria alocada:', process.memoryUsage().heapUsed / 1024 / 1024, 'MB');
 
   //todo: trocar para estrutura de classe para trabalhar com liberação de ponteiro
-  //e analisar uso do GC 
-  //data = [];
-}
+  //e analisar uso do GC
+  data = [];
+};
 
 const memoryLeak = () => {
   //Controle de uso da CPU a partir do controle do tempo para trabalho de join join e cópia da matriz (x)
   setInterval(callbackMemoryLeak, temporizador);
 };
 
-const StartServer = async() => {
-
+const StartServer = async () => {
   const app = express();
-  app.listen(8001, () => {
-    console.log(`listening to port 8001`);
-  })
-  .on('error', (err) => {
+  app
+    .listen(8001, () => {
+      console.log(`listening to port 8001`);
+    })
+    .on("error", (err) => {
       console.log(err);
       process.exit();
-  })
+    });
 
-  app.get('/geti', (req,res) => {
-    return res.status(200).json({msg: '/geti : I am Customer Service'})
-  })
-
-}
+  app.get("/geti", (req, res) => {
+    return res.status(200).json({ msg: "/geti : I am Customer Service" });
+  });
+};
 
 //Work 1 - Eventloop latency => 80.000,00 ms
 //var tamanhoArray = 1000000;
@@ -45,7 +43,7 @@ const StartServer = async() => {
 //var maxIterador = 10;
 //var temporizador = 60000;
 
-//Teste 3 
+//Teste 3
 var tamanhoArray = 10000;
 var maxIterador = 10000;
 var temporizador = 500;
---
+++ memoryleak/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Obter a data e hora atual
+current_datetime=$(date +"%Y%m%d_%H%M%S")
+
+# Caminho para o arquivo Node.js que você deseja executar
+node_script="./src/leak.js"
+
+# Nome do arquivo de log com data e hora
+log_file="./trace/${current_datetime}_trace-gc.out"
+
+# Verifique se o arquivo Node.js existe
+if [ -f "$node_script" ]; then
+    # Execute o arquivo Node.js e redirecione a saída para o arquivo de log
+    echo "Script leak.js executando e log gerado em $log_file"
+    node --trace-gc "$node_script" > "$log_file" 2>&1
+else
+    echo "Arquivo Node.js não encontrado: $node_script"
+fi
